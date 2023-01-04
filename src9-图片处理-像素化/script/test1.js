e8d0a42a7b6431e790fbfcb0ca5c679394e42fd0
薛江平 <1127523185@qq.com>
2023-01-04 17:30:26 +0800

    1

--- src9-图片处理-像素化/script/test1.js
+++ src9-图片处理-像素化/script/test1.js
@@ -112,4 +112,19 @@ class Demo2 {
 }
 // demo = new Demo2(v).init(300,200,200,100 + v.clientHeight)
 
-debugger
+async function a(myPath) {
+  const dirArr = await readdir(myPath)
+  const reg = /^src\d/
+  const reg2 = /(?<=<body>)(.|\n|\r)*?(?=<\/body>)/
+  let res = dirArr.reduce((t,v) => reg.test(v) ? `${t}<h1><a href="./${v}">${v}</a></h1>\n` : t,'\n')
+  let data = await readFile(myPath + 'index.html','utf-8')
+  let newData = data.replace(reg2,res)
+  await writeFile(myPath + 'index.html',newData)
+  // console.log('写入完成')
+}
+a('./')
+
+
+
+
+// debugger
