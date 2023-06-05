7a53b91e85591a4af397b224a972f53c099895c4
薛江平 <1127523185@qq.com>
2023-06-05 15:04:23 +0800

    1

--- utils/m1.js
+++ utils/m1.js
@@ -1,48 +1,54 @@
 // 将模块 绑定到全局对象
 (function (root,factory) {
   console.log('没有模块环境，直接挂载在全局对象上')
+  if (!root) return console.error('未找到全局对象')
   root.umdModule = factory();
-}(this || globalThis || window,function () {
-  // 寻找元素
-  const findElement = ((lastDoc,frames) => {
-    return function (selectors,isAll = false) {
-      let res,doc
-      if (lastDoc) {
-        if (isAll) {
-          res = lastDoc.querySelectorAll(selectors)
-          if (res.length > 0) return res
-        } else {
-          res = lastDoc.querySelector(selectors)
-          if (res) return res
-        }
-      }
-      for (let i = 0; i < frames.length; i++) {
-        try {
-          doc = frames[i].document
-        } catch (error) {
-          continue
-        }
-        if (isAll) {
-          res = doc.querySelectorAll(selectors)
-          if (res.length > 0) {
-            lastDoc = doc
-            return res
-          }
-        } else {
-          res = doc.querySelector(selectors)
-          if (res) {
-            lastDoc = doc
-            return res
-          }
-        }
-      }
-      console.log('未找到元素',selectors)
-    }
-  })(window.document,window.frames);
-  // 记录日志
-  const log = (...msg) => console.log(`[${new Date().toLocaleTimeString()}]:`,...msg)
+}(typeof this === 'object' ? this
+  : typeof global === 'object' ? global
+    : typeof globalThis === 'object' ? globalThis
+      : typeof window === 'object' ? window : null,
+  function () {
 
-  return {
-    findElement,log
-  }
-}))
+    // 寻找元素
+    const findElement = ((lastDoc,frames) => {
+      return function (selectors,isAll = false) {
+        let res,doc
+        if (lastDoc) {
+          if (isAll) {
+            res = lastDoc.querySelectorAll(selectors)
+            if (res.length > 0) return res
+          } else {
+            res = lastDoc.querySelector(selectors)
+            if (res) return res
+          }
+        }
+        for (let i = 0; i < frames.length; i++) {
+          try {
+            doc = frames[i].document
+          } catch (error) {
+            continue
+          }
+          if (isAll) {
+            res = doc.querySelectorAll(selectors)
+            if (res.length > 0) {
+              lastDoc = doc
+              return res
+            }
+          } else {
+            res = doc.querySelector(selectors)
+            if (res) {
+              lastDoc = doc
+              return res
+            }
+          }
+        }
+        console.log('未找到元素',selectors)
+      }
+    })(window.document,window.frames);
+    // 记录日志
+    const log = (...msg) => console.log(`[${new Date().toLocaleTimeString()}]:`,...msg)
+
+    return {
+      findElement,log
+    }
+  }))
